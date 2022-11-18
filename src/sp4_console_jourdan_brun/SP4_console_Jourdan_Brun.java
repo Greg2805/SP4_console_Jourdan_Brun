6527d29fb8258f58f0b9b1c3fe1095d02b7f7f9e
Gregoire Brun <gregoire.brun@epfedu.fr>
2022-11-18 10:46:55 +0100

    création d'un nouveau projet SP4

---
+++ src/sp4_console_jourdan_brun/SP4_console_Jourdan_Brun.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sp4_console_jourdan_brun;
+
+/**
+ *
+ * @author grego
+ */
+public class SP4_console_Jourdan_Brun {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
