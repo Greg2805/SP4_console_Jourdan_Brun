f7f7259b9971e186300b6decfc6a251cc4939069
Gregoire Brun <gregoire.brun@epfedu.fr>
2022-11-18 11:35:26 +0100

    classe CelluleDeGrille créée

---
+++ src/sp4_console_jourdan_brun/CelluleDeGrille.java
@@ -0,0 +1,24 @@
+package sp4_console_jourdan_brun;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author grego
+ */
+public class CelluleDeGrille {
+    private Jeton jetonCourant;
+    
+    public void CelluleDeGrille (){
+        jetonCourant=null;
+    }
+    
+    public boolean presenceJeton (){
+        if (jetonCourant =! null){
+            
+        }
+    }
+}
