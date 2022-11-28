fc68bf43f3c7aaf5661d7bca8c44d32053650871
Gregoire Brun <gregoire.brun@epfedu.fr>
2022-11-28 15:30:52 +0100

    classe partie ajout de jeton fait

--- src/sp4_console_jourdan_brun/Partie.java
+++ src/sp4_console_jourdan_brun/Partie.java
@@ -4,14 +4,54 @@
  */
 package sp4_console_jourdan_brun;
 
+import java.util.Random;
+
 /**
  *
  * @author grego
  */
 public class Partie {
-    private int [] listeJoueurs = new int[2]; 
+    private Joueur [] listeJoueurs = new Joueur[2]; 
     private Joueur joueurCourant;
     private PlateauDeJeu plateau;
+    
+    public void Partie(Joueur joueur1, Joueur joueur2){
+        listeJoueurs [0]=joueur1;
+        listeJoueurs [1]=joueur2;
+    }
+    
+    public void attribuerCouleurAuxJoueurs (){
+        Random generateurAleat = new Random();
+        int n = generateurAleat.nextInt(1);
+        int temp = 0;
+        
+        if (n==1){
+            listeJoueurs[0].affecterCouleur("rouge");
+            temp =0;
+        }
+        else{
+            listeJoueurs[0].affecterCouleur("jaune");
+            temp=1; 
+        }
+        
+        if(temp==0){
+            listeJoueurs[1].affecterCouleur("jaune");
+        }
+        else{
+        listeJoueurs[1].affecterCouleur("rouge");
+        }
+        
+    }
+    
+    public void creerEtAffecterJeton (Joueur j_jeton){
+        String clr_j = j_jeton.donnerCouleur();
+        
+         for (int i =0 ;i<=30;i++){
+             
+             j_jeton.ajouterJeton(new Jeton(clr_j));
+             
+         }
+    }
     
     
 }
