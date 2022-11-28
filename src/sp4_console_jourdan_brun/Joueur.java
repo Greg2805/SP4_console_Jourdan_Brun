8981c8a3db15709525bc3a0564ec7a7b60e2bb02
Gregoire Brun <gregoire.brun@epfedu.fr>
2022-11-28 15:33:29 +0100

    classe partie ajout de jeton fait

--- src/sp4_console_jourdan_brun/Joueur.java
+++ src/sp4_console_jourdan_brun/Joueur.java
@@ -11,32 +11,45 @@ import java.util.ArrayList;
  * @author grego
  */
 public class Joueur {
+    
     private String nom;
     private String couleur;
     private ArrayList<Jeton> reserveJetons;
     
+    
     public void Joueur (){
     
-        nom="Matthieuuuuuuuuuuuu";
-        couleur="couleur";
+        nom="nom joueur pas enccore entré";
+        couleur="couleur non initialisée";
     }
+    
     
     public void affecterCouleur(String clr_s){
         couleur=clr_s;
     }
+    
+    public String donnerCouleur(){
+        return couleur;
+    }
+    
     
     public int nombreDeJetons(){
         int taille = reserveJetons.size();
         return taille;
     }
     
+    
     public void ajouterJeton(Jeton ajout){
         reserveJetons.add(ajout);
     }
+    
     
     public Jeton jouerJeton (){
         int a = nombreDeJetons();
         Jeton j = reserveJetons.remove(a);
         return j;
     }
+   
+    
 }
+
