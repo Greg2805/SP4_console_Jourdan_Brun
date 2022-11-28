a3272b6aecfdd45798474d311ed7ecfc4ed4185f
Gregoire Brun <gregoire.brun@epfedu.fr>
2022-11-28 14:17:08 +0100

    début classe partie

---
+++ src/sp4_console_jourdan_brun/Partie.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sp4_console_jourdan_brun;
+
+/**
+ *
+ * @author grego
+ */
+public class Partie {
+    private int [] listeJoueurs = new int[2]; 
+    private Joueur joueurCourant;
+    private PlateauDeJeu plateau;
+    
+    
+}
