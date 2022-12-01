841dfede3182d65afee376215cbcc83338f2dba2
Dell <Dell@DESKTOP-VJLVIIN.education.lakanal.epf.fr>
2022-12-01 18:58:45 +0100

    SP4 fin trou noire/desintégrateur

--- src/sp4_console_jourdan_brun/Joueur.java
+++ src/sp4_console_jourdan_brun/Joueur.java
@@ -11,35 +11,46 @@ import java.util.ArrayList;
  * @author grego
  */
 public class Joueur {
+    
     private String nom;
     private String couleur;
     private ArrayList<Jeton> reserveJetons;
     private int nombreDesintegrateurs;
     
+    
     public void Joueur (){
     
-        nom="Matthieuuuuuuuuuuuu";
-        couleur="couleur";
+        nom="nom joueur pas enccore entré";
+        couleur="couleur non initialisée";
     }
+    
     
     public void affecterCouleur(String clr_s){
         couleur=clr_s;
     }
+    
+    public String donnerCouleur(){
+        return couleur;
+    }
+    
     
     public int nombreDeJetons(){
         int taille = reserveJetons.size();
         return taille;
     }
     
+    
     public void ajouterJeton(Jeton ajout){
         reserveJetons.add(ajout);
     }
+    
     
     public Jeton jouerJeton (){
         int a = nombreDeJetons();
         Jeton j = reserveJetons.remove(a);
         return j;
     }
+
     
     public void obtenirDesintegrateur(){
         nombreDesintegrateurs +=1;
@@ -47,5 +58,11 @@ public class Joueur {
     
     public void utiliserDesintegrateur(){
         nombreDesintegrateurs -=1;
+        
     }
+
+   
+    
+
 }
+
